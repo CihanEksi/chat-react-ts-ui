a8cff918ea9468e22c01f4e066f0095dcfe4c2ba
Cihan-Frupro <cihan.eksioglu@frupro.com>
2024-07-15 18:53:51 +0300

    login redirect if user already logged in

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -2,9 +2,19 @@ import { Link } from "react-router-dom";
 import Auth from "./Auth";
 import { Link as MULink } from "@mui/material";
 import { useLogin } from "../../hooks/useLogin";
+import useGeneralStore from "../../managers/stateManager/general.zustand";
+import { useEffect } from "react";
+import router from "../../routes/Routes";
 
 const Login = () => {
   const { login, error, errorMessage } = useLogin();
+  const me = useGeneralStore((state) => state.me);
+
+  useEffect(() => {
+    if (me) {
+      router.navigate("/");
+    }
+  }, [me]);
 
   return (
     <div>
