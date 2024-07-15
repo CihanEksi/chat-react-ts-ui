3fbd0cbd174f48fc9f707ea595b54702bd16471f
Cihan-Frupro <cihan.eksioglu@frupro.com>
2024-07-15 18:24:50 +0300

    added comments to guard component

--- src/components/Auth/Guard.tsx
+++ src/components/Auth/Guard.tsx
@@ -9,18 +9,18 @@ interface GuardProps {
 const Guard = ({ children }: GuardProps) => {
   const me = useGeneralStore((state) => state.me);
   const includedCurrentRoute = excludedRoutes.includes(
-    window.location.pathname
+    window.location.pathname // we are using window to get the current path because Guard is a top-level component
   );
   const isUserLoggedIn = me !== null;
   const managedCondition = !includedCurrentRoute && !isUserLoggedIn;
 
   useEffect(() => {
     if (managedCondition) {
-      window.location.href = "/login";
+      window.location.href = "/login"; // we are using windo to redirect the user to the login page becase Guard is a top-level component
     }
   }, [managedCondition]);
 
-  if (managedCondition) {
+  if (managedCondition && !includedCurrentRoute) {
     return <></>; // Return nothing if the condition is met and avoid rendering the children
   }
 
